8b8f7ba96e577362d5fe9da0a2513a9da3c43f82
Prasanth280202 <162082672+Prasanth280202@users.noreply.github.com>
2025-03-31 20:22:17 +1030

    Update Proxy.py

--- Proxy.py
+++ Proxy.py
@@ -9,9 +9,9 @@ size = 1000000
 parser = argparse.ArgumentParser()
 parser.add_argument('hostname', help='the IP Address Of Proxy Server')
 parser.add_argument('port', type=int, help='the port number of the proxy server')
-args = parser.parse_args()
-proxyHost = args.hostname
-proxyPort = args.port
+arg = parser.parse_args()
+proxyHost = arg.hostname
+proxyPort = arg.port
 
 try:
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
